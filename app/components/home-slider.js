20d55a714c9f130dbfbac099e77381edf6976ed0
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2016-12-18 22:01:32 +0545

    Enhance home with footer and background

--- app/components/home-slider.js
+++ app/components/home-slider.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+   
     didRender(){
         this._super(...arguments);
-        this.$('.button-collapse').sideNav();
+        this.$('.carousel').carousel();
+        //this.$('.carousel.carousel-slider').carousel({full_width: true});
+        
     }
 
 });
