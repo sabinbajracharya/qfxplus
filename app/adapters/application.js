1fdc415228bf7f5e0c8b203368587edc242e1630
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2016-12-21 23:33:31 +0545

    change sliding drawer menus

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
     //host: 'http://sabin.wlink.com.np',
-    host: 'http://localhost',
-    //namespace: 'apiv1'
-    namespace: 'qfx/index.php/apiv1'
+    //host: 'http://localhost',
+    namespace: 'apiv1'
+    //namespace: 'qfx/index.php/apiv1'
 });
