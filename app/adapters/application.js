42316c4d18f24fdb4e8b52bd621a39b82dc9f69a
Sabin Bajracharya <sabin.bajracharya@gmail.com>
2016-12-22 14:18:49 +0545

    Change api server

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-    //host: 'http://sabin.wlink.com.np',
+    host: 'http://sabin.wlink.com.np',
     //host: 'http://localhost',
-    namespace: 'apiv1'
-    //namespace: 'qfx/index.php/apiv1'
+    //namespace: 'apiv1'
+    namespace: 'qfx/index.php/apiv1'
 });
